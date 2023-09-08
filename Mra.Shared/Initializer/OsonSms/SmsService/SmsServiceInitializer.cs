927fb1857c81b7ef352b117ab65d942f79303674
Azimjon Fayzulloev <sherman.peabody99@gmail.com>
2023-09-08 10:52:11 +0500

    added test sender

--- Mra.Shared/Initializer/OsonSms/SmsService/SmsServiceInitializer.cs
+++ Mra.Shared/Initializer/OsonSms/SmsService/SmsServiceInitializer.cs
@@ -1,12 +1,16 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Mra.Shared.Common.Interfaces.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Mra.Shared.Initializer.OsonSms.SmsService
+namespace Mra.Shared.Initializer.OsonSms.SmsService;
+public static class SmsServiceInitializer
 {
-    internal class SmsServiceInitializer
+    public static void AddOsonSmsService(this IServiceCollection services)
     {
+        services.AddScoped<ISmsService, Shared.OsonSms.SmsService.SmsService>();
     }
 }
--- Mra.Shared/OsonSms/SmsService/SmsService.cs
+++ Mra.Shared/OsonSms/SmsService/SmsService.cs
@@ -1,12 +1,69 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Microsoft.Extensions.Logging;
+using Mra.Shared.Common.Constants;
+using Mra.Shared.Common.Interfaces.Services;
+using System.Security.Cryptography;
 using System.Text;
-using System.Threading.Tasks;
 
-namespace Mra.Shared.OsonSms.SmsService
+namespace Mra.Shared.OsonSms.SmsService;
+public class SmsService : ISmsService
 {
-    internal class SmsService
+    private readonly HttpClient _client;
+    private readonly ILogger<SmsService> _logger;
+
+    public SmsService(HttpClient client, ILogger<SmsService> logger)
     {
+        _client = client;
+        _logger = logger;
+    }
+    public async Task<int> SendSmsAsync(string phoneNumber)
+    {
+        int code;
+        var config = new Dictionary<string, string>();
+        config["dlm"] = ";"; // do not change!!! 
+        config["t"] = "23"; // do not change!!!
+
+        config["login"] = ConfigurationKeys.OsonSmsLogin; // Your login
+        config["pass_hash"] = ConfigurationKeys.OsonSmsPassHash; // Your hash code
+        config["sender"] = ConfigurationKeys.OsonSmsSender; // Your alphanumeric
+
+        var txn_id = (int)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds; // Has to be unique for each request
+
+        var str_hash = Sha256Hash(txn_id + config["dlm"] + config["login"] + config["dlm"] + config["sender"] + config["dlm"] + phoneNumber + config["dlm"] + config["pass_hash"]);
+
+        string url = $"https://api.osonsms.com/sendsms_v1.php?login={config["login"]}&from={config["sender"]}&phone_number={phoneNumber}&msg={GenerateMessage(out code)}&txn_id={txn_id}&str_hash={str_hash}";
+
+        try
+        {
+            await _client.GetAsync(url);
+            return code;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error on sending sms");
+            return -1;
+        }
+    }
+
+    private static String Sha256Hash(String value)
+    {
+        StringBuilder Sb = new StringBuilder();
+
+        using (SHA256 hash = SHA256Managed.Create())
+        {
+            Encoding enc = Encoding.UTF8;
+            Byte[] result = hash.ComputeHash(enc.GetBytes(value));
+
+            foreach (Byte b in result)
+                Sb.Append(b.ToString("x2"));
+        }
+
+        return Sb.ToString();
+    }
+
+    private static String GenerateMessage(out int code)
+    {
+        Random random = new Random();
+        code = random.Next(1000, 10001);
+        return $"Your confirmation code is: {code}. Please enter this code to verify your phone number.";
     }
 }
