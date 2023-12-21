ec247e384a037eb1dcc8166460ffa06d229558d3
adheeeem <sherman.peabody99@gmail.com>
2023-12-21 17:18:27 +0500

    added docs

--- MRA.Configurations/Initializer/Azure/AppConfig/ConfigureAzureAppConfig.cs
+++ MRA.Configurations/Initializer/Azure/AppConfig/ConfigureAzureAppConfig.cs
@@ -1,16 +1,20 @@
 ﻿using Microsoft.Extensions.Configuration;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MRA.Configurations.Initializer.Azure.AppConfig
 {
+    /// <summary>
+    /// Extension methods for configuring Azure App Configuration in the application.
+    /// </summary>
     public static class ConfigureAzureAppConfig
     {
+        /// <summary>
+        /// Adds Azure App Configuration to the configuration manager.
+        /// </summary>
+        /// <param name="configurations">The <see cref="ConfigurationManager"/> instance.</param>
+        /// <param name="connectionString">The Azure App Configuration connection string.</param>
         public static void AddAzureAppConfig(this ConfigurationManager configurations, string connectionString)
         {
+            // Connects to Azure App Configuration using the provided connection string.
             configurations.AddAzureAppConfiguration(options => options.Connect(connectionString));
         }
     }
