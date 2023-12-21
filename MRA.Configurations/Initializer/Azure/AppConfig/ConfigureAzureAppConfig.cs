463ce8fd0e1b454962b6fc80bc1a877fb76b25c0
adheeeem <sherman.peabody99@gmail.com>
2023-12-21 17:16:52 +0500

    added azure app config

---
+++ MRA.Configurations/Initializer/Azure/AppConfig/ConfigureAzureAppConfig.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MRA.Configurations.Initializer.Azure.AppConfig
+{
+    public static class ConfigureAzureAppConfig
+    {
+        public static void AddAzureAppConfig(this ConfigurationManager configurations, string connectionString)
+        {
+            configurations.AddAzureAppConfiguration(options => options.Connect(connectionString));
+        }
+    }
+}
